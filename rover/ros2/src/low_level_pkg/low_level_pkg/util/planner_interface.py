1b29a89714c3bfa70c74e3ed709cec80317fa333
Diego Carvajal <diego98cs@gmail.com>
2023-02-05 22:52:38 +0000

    Get waypoints from file function

--- rover/ros2/src/low_level_pkg/low_level_pkg/util/planner_interface.py
+++ rover/ros2/src/low_level_pkg/low_level_pkg/util/planner_interface.py
@@ -27,10 +27,10 @@ class PlannerInterface:
         self.logger = parent_node.get_logger()
         self.planner_server_client = ActionClient(parent_node, ComputePathThroughPoses, "compute_path_through_poses")
 
-    def call_action_client(self, frame_id: str, poses: List[PoseStamped], start: PoseStamped, planner_id: str, use_start: bool):
+    def call_action_client(self, frame_id: str, goals: List[PoseStamped], start: PoseStamped, planner_id: str, use_start: bool):
         """! Call the planner server action to calculate a path.
 
-        @param poses "List[PoseStamped]" list of PoseStamped elements to plan the path
+        @param goals "List[PoseStamped]" list of PoseStamped elements to plan the path
         @param start "PoseStamped" PoseStamped element that defines the start of the path
         @param planner_id "str" Planner to be used
         @param use_start "bool" If set to False, start from the actual position of the robot,
@@ -76,7 +76,46 @@ class PlannerInterface:
         return future.result()
 
 
-def # TODO:
+def read_waypoints():
+    """! Function that generates a waypoints to test the planner server.
+    @return "str" name of the frame for the path.
+    @return "List[PoseStamped]" list of PoseStamped elements with the goal
+        waypoints.
+    @return "PoseStamped" PoseStamped element with the start of the path
+    @return "str" name of the used path planner.
+    @return "bool" if set to False, use robot's current pose as path start.
+    """
+
+    frame_id = "map"
+
+    with open("/workspace/rover/ros2/src/tb_bringup/config/waypoints.yaml") as f:
+        waypoints = yaml.safe_load(f)
+        waypoints = waypoints["waypoints"]
+
+    keys = list(waypoints.keys())
+
+    goals = []
+    pose_msg = PoseStamped()
+
+    # Define goal pose
+    pose_msg.pose.position.x = waypoints[keys[0]]["pose"]["x"]
+    pose_msg.pose.position.y = waypoints[keys[0]]["pose"]["y"]
+
+    goals.append(pose_msg)
+
+    # Define start pose
+    pose_msg.pose.position.x = waypoints[keys[1]]["pose"]["x"]
+    pose_msg.pose.position.y = waypoints[keys[1]]["pose"]["y"]
+
+    start = pose_msg
+
+    # Define planner_id
+    planner_id = "GridBased"
+
+    # Use start pose for path planning
+    use_start = True
+
+    return frame_id, goals, start, planner_id, use_start
 
 
 def test_planner_server(args=None):
