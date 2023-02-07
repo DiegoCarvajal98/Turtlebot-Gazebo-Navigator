79c77a070d7ef46ef9434a961e27c805f94dd8a9
Diego Carvajal <diego98cs@gmail.com>
2023-02-07 03:11:10 +0000

    Read waypoints from yaml file function

--- rover/ros2/src/high_level_nav2_commander_py/high_level_nav2_commander_py/high_level_commander_node.py
+++ rover/ros2/src/high_level_nav2_commander_py/high_level_nav2_commander_py/high_level_commander_node.py
@@ -2,12 +2,29 @@ import rclpy
 from rclpy.node import Node
 from rclpy.executors import MultiThreadedExecutor
 
+import ament_index_python
+
+import yaml
 
 class Nav2CommanderNode(Node):
     def __init__(self, node_name):
         Node.__init__(self, node_name=node_name)
         self.get_logger().info("Hi! I'm the nav2 commander node!")
 
+    def read_waypoints(self, pkg: str, folder: str):
+        """! Function to read the waypoints from a yaml file
+        @param pkg "str" Package where the yaml file is located
+        @param folder "str" Yaml file location folder within the package
+
+        @return "dict" Waypoints from the yaml file
+        """
+        dir = ament_index_python.get_package_prefix(pkg)
+        dir = dir + folder
+        
+        with open(dir) as f:
+            waypoints = yaml.safe_load(f)["waypoints"]
+
+        return waypoints
 
 def main():
     rclpy.init()
