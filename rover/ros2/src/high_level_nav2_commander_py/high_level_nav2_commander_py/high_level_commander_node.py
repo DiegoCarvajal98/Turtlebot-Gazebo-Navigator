12187202ae00181eb23d42b60956064b6eba9532
Diego Carvajal <diego98cs@gmail.com>
2023-02-07 03:11:20 +0000

    Read waypoints from yaml file function

--- rover/ros2/src/high_level_nav2_commander_py/high_level_nav2_commander_py/high_level_commander_node.py
+++ rover/ros2/src/high_level_nav2_commander_py/high_level_nav2_commander_py/high_level_commander_node.py
@@ -1,6 +1,10 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.executors import MultiThreadedExecutor
+from rclpy.action import ActionClient
+
+# Import ROS interfaces
+from nav2_msgs.action import NavigateToPose
 
 import ament_index_python
 
@@ -10,6 +14,7 @@ class Nav2CommanderNode(Node):
     def __init__(self, node_name):
         Node.__init__(self, node_name=node_name)
         self.get_logger().info("Hi! I'm the nav2 commander node!")
+        
 
     def read_waypoints(self, pkg: str, folder: str):
         """! Function to read the waypoints from a yaml file
